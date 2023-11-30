9bb02af1231bfd06c79166680fc3b613cef06c17
skashequr <learningwithextracare@gmail.com>
2023-11-30 23:00:44 +0600

    new  route

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 app.use(
   cors({
-    origin: ["http://localhost:5173",],
+    origin: ["http://localhost:5173","https://employ-management-20d5c.web.app/"],
     credentials: true,
   })
 );
@@ -233,8 +233,8 @@ app.get('/workSheetHrPagination', async (req, res) => {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
